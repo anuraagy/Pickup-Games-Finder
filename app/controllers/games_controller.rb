21e4fdaa4e351f894f4e3272ef58fefe7c675241
jessica-tian <jessicatian1@gmail.com>
2018-04-14 16:37:27 -0400

    Add create and edit functionality for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,52 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @game = Game.all
+
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to @game
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+
+    if @game.update(game_params)
+      redirect_to @game
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def destroy
+    @game = Game.find(params[:id]) 
+
+    @game.destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:address, :team_one_name, :team_two_name, :team_one_score, :team_two_score, :creator_id, :state)
+  end
 end
