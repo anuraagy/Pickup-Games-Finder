ae9743eb21c628ac1a369b675c2203def548d784
jessica-tian <jessicatian1@gmail.com>
2018-04-17 00:41:48 -0400

    Add styling to login, registration, and password reset controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, :except => [:index]
+
   def index
-    @games = Game.not_started
-    @games_hash = Gmaps4rails.build_markers(@games) do |game, marker|
-      marker.lat game.latitude
-      marker.lng game.longitude
+    if current_user
+      @games = Game.not_started
+      @games_hash = Gmaps4rails.build_markers(@games) do |game, marker|
+        marker.lat game.latitude
+        marker.lng game.longitude
+      end
     end
   end
 
