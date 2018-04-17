84f837b94643d1193dae05fb2f3ff74cf9013763
Evett <pwrammerfors@yahoo.com>
2018-04-17 18:47:44 -0400

    Continue working on game front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-100.times do |index|
-  user_params = {
+user_params = {
     :name               => Faker::Name.unique.name, 
     :email              => Faker::Internet.unique.email, 
     :password           => "password",
     :uname              => SecureRandom.urlsafe_base64(24),
   }
 
-  user = User.create!(user_params)
+user = User.create!(user_params)
 
+1000.times do |index|  
   game_params = {
-    :title               => Faker::Name.unique.name, 
-    :description         => Faker::Internet.unique.email, 
+    :title               => "Game #{index}", 
+    :description         => Faker::Lorem.paragraph, 
     :creator_id          => 1,
     :address             => "#{Faker::Address.street_address} #{Faker::Address.city} #{Faker::Address.state} #{Faker::Address.zip}",
     :team_one_name       => Faker::Team.name,
@@ -18,8 +18,8 @@
     :team_one_score      => rand(0..100),
     :team_two_score      => rand(0..100),
     :start_time          => Time.now.tomorrow,
-    :latitude            => Faker::Address.latitude,
-    :longitude           => Faker::Address.longitude
+    :latitude            => rand(30.50139...64.85694),
+    :longitude           => rand(-115.75583..-80.01197)
   }
 
   Game.create(game_params)
